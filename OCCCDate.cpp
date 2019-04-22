1f710d84d1eb332dd32fc16deab7930a483dc986
Cody Mumford <cody.mumford9106@hotmail.com>
2019-04-21 20:40:30 -0500

    updated getNameOfMonth()

--- OCCCDate.cpp
+++ OCCCDate.cpp
@@ -49,40 +49,40 @@ std::string OCCCDate::getNameOfMonth(int id)
 
 	switch (id)
 	{
-	case 0: Month = "January";
+	case 1: Month = "January";
 		break;
 
-	case 1: Month = "February";
+	case 2: Month = "February";
 		break;
 
-	case 2: Month = "March";
+	case 3: Month = "March";
 		break;
 
-	case 3: Month = "April";
+	case 4: Month = "April";
 		break;
 
-	case 4: Month = "May";
+	case 5: Month = "May";
 		break;
 
-	case 5: Month = "June";
+	case 6: Month = "June";
 		break;
 
-	case 6: Month = "July";
+	case 7: Month = "July";
 		break;
 
-	case 7: Month = "August";
+	case 8: Month = "August";
 		break;
 
-	case 8: Month = "September";
+	case 9: Month = "September";
 		break;
 
-	case 9: Month = "October";
+	case 10: Month = "October";
 		break;
 
-	case 10: Month = "November";
+	case 11: Month = "November";
 		break;
 
-	case 11: Month = "December";
+	case 12: Month = "December";
 		break;
 	}
 	return Month;
